be1a2ced8ceda4bd8144b078525a3fbea796bd1e
Nate Miller <natekmiller96@gmail.com>
2018-06-25 19:34:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,9 +65,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holiday|
+  holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
-    season.each do |holiday, supplies|
+    holidays.each do |holiday, supplies|
       puts "  #{holiday}: #{supplies.join(",")}"
     end
   end
