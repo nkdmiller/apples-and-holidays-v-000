361553e9abb9f6e5eb08abad9e2616f2586b6c9b
Nate Miller <natekmiller96@gmail.com>
2018-06-25 18:57:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holiday|
     if season == :winter
       season.each do |holiday, supplies|
-        winter_supplies_hash << [holiday][supplies]
+        winter_supplies_hash << holiday.values
       end
     end
   end
